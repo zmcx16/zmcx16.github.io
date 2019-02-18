7b608eb955f2468a84706a5b7d0733dda987c21b
zmcx16 <zmcx16@gmail.com>
2019-02-18 22:28:02 +0800

    update illustration img

---
+++ js/common.js
@@ -0,0 +1,11 @@
+const illustration_img_home = 'illustration.png';
+const illustration_img_mahomanga = 'illustration_mahomanga.png';
+
+function pickImg(img_ID, img_List){
+
+
+}
+
+function loadIllustrationImg(file_name) {
+    $(".top-shelf-grey .container")[0].style.background = 'url(/img/' + file_name + ') no-repeat right bottom';
+}
