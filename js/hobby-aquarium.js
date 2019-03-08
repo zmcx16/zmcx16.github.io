84466a39cfe8a5d1c42484475b6f09d82a58a1c4
zmcx16 <zmcx16@gmail.com>
2019-03-09 02:31:06 +0800

    Fix repo link bug, fix wrong class name

--- js/hobby-aquarium.js
+++ js/hobby-aquarium.js
@@ -2,4 +2,4 @@ var myvar = '<div class=\'main-hobby_aquarium\'>' +
     '    <h1>網站建置中...</h1>' +
     '</div>';
 
-document.getElementById('main-hobby_aquarium-wrap').innerHTML = myvar;
+document.getElementById('main-plugin-wrap').innerHTML = myvar;
--- js/hobby-trade.js
+++ js/hobby-trade.js
@@ -2,4 +2,4 @@ var myvar = '<div class=\'main-hobby_trade\'>' +
     '    <h1>網站建置中...</h1>' +
     '</div>';
 
-document.getElementById('main-hobby_trade-wrap').innerHTML = myvar;
+document.getElementById('main-plugin-wrap').innerHTML = myvar;
