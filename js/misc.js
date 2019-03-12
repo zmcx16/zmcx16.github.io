0e3c5c43fc91534e1d5108f60d24edcd9de227a6
zmcx16 <zmcx16@gmail.com>
2019-03-13 00:33:01 +0800

    small implement for misc page

--- js/misc.js
+++ js/misc.js
@@ -1,5 +1,29 @@
-var myvar = '<div class=\'main-misc\'>' +
-    '    <h1>網站建置中...</h1>' +
+
+var myvar = '<link rel="stylesheet" type="text/css" href="css/Misc.css">' +
+    '<div class=\'main-misc\'>' +
+    '    <div class="misc-show">' +
+    '        <img class="img-misc" id="img-misc0" src="/img/MahoMangaDownloader/manga1.png" />' +
+    '        <img class="img-misc" id="img-misc1" src="/img/MahoMangaDownloader/manga2.png" />' +
+    '        <img class="img-misc" id="img-misc2" src="/img/MahoMangaDownloader/manga3.png" />' +
+    '    </div>' +
     '</div>';
 
 document.getElementById('main-plugin-wrap').innerHTML = myvar;
+
+const img_num = 3;
+var demo_img_now = 0;
+document.getElementById("img-misc" + demo_img_now).style = "position:relative; opacity:1; display: block";
+
+function changeDemoImg() {
+    var previous_img_index = demo_img_now;
+    demo_img_now = (demo_img_now + 1) % img_num;
+
+    $("#img-misc" + previous_img_index).fadeTo(800, 0.05, function () {
+        document.getElementById("img-misc" + previous_img_index).style = "position:absolute; opacity:0; display: none";
+        document.getElementById("img-misc" + demo_img_now).style = "position:relative; opacity:0.05; display: block";
+        $("#img-misc" + demo_img_now).fadeTo(800, 1);
+    });
+
+}
+
+setInterval(changeDemoImg, 7000);
