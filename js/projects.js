a2c050ff65d261e655132267c4f526b20d10b49b
zmcx16 <zmcx16@gmail.com>
2019-11-10 01:04:44 +0800

    add TW-Stock Lottery Secretary side project

--- js/projects.js
+++ js/projects.js
@@ -44,18 +44,25 @@ oss_projects['OpenAI-Gym-Hearts'] = {
   background: 'OpenAI-Gym-Hearts_295x150.jpg',
   msg: '要一起玩牌嗎?'
 };
+oss_projects['TW-Stock Lottery Secretary'] = {
+  blog_post: 'https://blog.zmcx16.moe/search/label/FB%E7%B2%89%E7%B5%B2%E5%B0%88%E9%A0%81%E6%A9%9F%E5%99%A8%E4%BA%BA%E9%96%8B%E7%99%BC%20-%20%E8%82%A1%E7%A5%A8%E6%8A%BD%E7%B1%A4%E5%B0%8F%E7%A7%98%E6%9B%B8',
+  featured: true,
+  position: 7,
+  background: 'TWStockLotterySecretary_295x150.jpg',
+  msg: '歡迎加入追蹤+訂閱喔~'
+};
 oss_projects['protobuf-deserializer'] = {
   blog_post: 'https://blog.zmcx16.moe/search/label/protobuf-deserializer%E7%B6%B2%E7%AB%99%E9%96%8B%E7%99%BC',
   online_website: 'https://protobuf-deserializer.zmcx16.moe',
   featured: true,
-  position: 7,
+  position: 8,
   background: 'protobuf-deserializer_295x150.jpg',
   msg: '好像很厲害?'
 };
 oss_projects['MemoOffVocabulary'] = {
   blog_post: 'https://blog.zmcx16.moe/search/label/MemoOffVocabulary',
   featured: true,
-  position: 8,
+  position: 9,
   background: 'MemoOffVocabulary_295x150.jpg',
   msg: '要好好念書喔~'
 };
@@ -63,19 +70,19 @@ oss_projects['zmcx16.github.io'] = {
   blog_post: 'https://blog.zmcx16.moe/search/label/%E5%80%8B%E4%BA%BA%E7%B6%B2%E7%AB%99%E9%96%8B%E7%99%BC',
   online_website: 'https://project.zmcx16.moe',
   featured: true,
-  position: 9,
+  position: 10,
   background: 'zmcx16.github.io_295x150.jpg',
   msg: 'いらっしゃいませ~~~'
 };
 oss_projects['OpenAI-Gym-GongZhu'] = {
   featured: true,
-  position: 10,
+  position: 11,
   background: 'OpenAI-Gym-GongZhu_295x150.jpg',
   msg: '要一起玩牌嗎?'
 };
 oss_projects['Misc'] = {
   featured: true,
-  position: 11,
+  position: 12,
   background: 'Misc_295x150.jpg',
   msg: '聽說是某個大哥哥的黑歷史?'
 };
