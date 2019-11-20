60655e8139708fca6759d9391c954df91e1318c6
zmcx16 <zmcx16@gmail.com>
2019-11-21 00:42:56 +0800

    add new support manga websites for mahomangadownloader

--- js/mahomangadownloader.js
+++ js/mahomangadownloader.js
@@ -1,128 +1,130 @@
 
-var myvar = '<link rel="stylesheet" type="text/css" href="css/mahomangadownloader.css">'+
-'<div class=\'main-MahoMangaDownloader\'>'+
-'    <div class="demo-img">'+
-'        <img src="/img/MahoMangaDownloader/MahoMangaDownloader_960x566.png"/>'+
-'    </div>'+
-'    <div class="description">'+
-'        <p> MahoMangaDownloader是一款支援多個漫畫網站的漫畫下載器, 提供使用者下載各個網站的漫畫資源, 方便使用者試看漫畫。 MahoMangaDownloader支援以下功能:<br></p>'+
-'            <ul class=\'description-function-list\'>'+
-'                <li>MahoMangaDownloader支援排程 & 多執行緒下載, 方便使用者在電腦空餘時間下載大量漫畫。</li>'+
-'                <li>多列表管理功能可讓使用者建立各類清單管理下載。</li>'+
-'                <li>搜尋功能讓使用者可以在資源網站搜尋作品或作者名, 直接取得相關聯的漫畫列表。</li>'+
-'                <li>追蹤功能可讓使用者建立追蹤清單, 只要執行掃描, 下載器就會去設定好的網址找出新更新或尚未下載的漫畫, 並自動加到設定好的下載清單。</li>'+
-'                <li>剪貼簿偵測功能可改善大量複製漫畫網址時的程序步驟。</li>'+
-'                <li>支援中英語系。</li>'+
-'                <li>線上自動更新功能。</li>'+
-'            </ul><br><br>'+
-'        <p> 針對特定網站, 也提供下列功能:<br></p>'+
-'            <ul class=\'description-function-list\'>'+
-'                <li>針對某些只限內部會員漫畫網站, MahoMangaDownloader也提供使用者設定帳密或cookie的機制支援下載。</li>'+
-'                <li>提供下載未壓縮過的原尺寸圖檔的功能。</li>'+
-'                <li>某些網站會針對下載器的行為進行IP封鎖, 針對這類網站MahoMangaDownloader也提供流量限制偵測以及自動換IP的功能(限動態網路環境下), 讓下載能繼續順利進行。</li>'+
-'            </ul><br><br>'+
-'        <p>希望MahoMangaDownloader下載器能幫助使用者改善線上漫畫的閱覽體驗, 如果試看的漫畫您非常喜歡, 也請麻煩購買正版支持原作者, 讓作家們能繼續創造出下一部更棒的作品。</p>'+
-'    </div>'+
-'    <div class="manga-show">'+
-'        <img class="img-manga" src="/img/MahoMangaDownloader/demo1/manga1.png" />'+
-'        <img class="img-manga" src="/img/MahoMangaDownloader/demo1/manga2.png" />'+
-'        <img class="img-manga" src="/img/MahoMangaDownloader/demo1/manga3.png" />'+
-'        <img class="img-manga" src="/img/MahoMangaDownloader/demo1/manga4.png" />'+
-'        <img class="img-manga" src="/img/MahoMangaDownloader/demo1/manga5.png" />'+
-'        <img class="img-manga" src="/img/MahoMangaDownloader/demo1/manga6.png" />'+
-'        <img class="img-manga" src="/img/MahoMangaDownloader/demo1/manga7.png" />'+
-'    </div>'+
-'    <div class="download-link">'+
-'        <h2>下載 MahoMangaDownloader{Version} [解壓密碼:zmcx16]</h2>'+
-'        <ul class=\'download-list\'>'+
-'            <li><a class="link" target="_blank" href="{FilePathX64}"><img src="/img/MahoMangaDownloader/DownloadStartButton_30x30.png"><p>{FileNameX64} ({FileSizeX64} MB)</p></a></li>'+
-'            <li><a class="link" target="_blank" href="{FilePathX86}"><img src="/img/MahoMangaDownloader/DownloadStartButton_30x30.png"><p>{FileNameX86} ({FileSizeX86} MB)</p></a></li>'+
-'        </ul>'+
-'    </div>'+
-'    <div class="manga-show-2">'+
-'        <div class="img-arrow-wrap">'+
-'            <div class="img-arrow-wrap-2">'+
-'                <img class="img-arrow" id="img-prev" src="/img/prev.png" />'+
-'                <img class="img-arrow" id="img-next" src="/img/next.png" />'+
-'            </div>'+
-'        </div>'+
-'        <div class="img-manga-2-wrap">'+
-'            <img class="img-manga-2" src="/img/MahoMangaDownloader/demo2/manga1.png" />'+
-'            <img class="img-manga-2" src="/img/MahoMangaDownloader/demo2/manga2.png" />'+
-'            <img class="img-manga-2" src="/img/MahoMangaDownloader/demo2/manga3.png" />'+
-'            <img class="img-manga-2" src="/img/MahoMangaDownloader/demo2/manga4.png" />'+
-'            <img class="img-manga-2" src="/img/MahoMangaDownloader/demo2/manga5.png" />'+
-'            <img class="img-manga-2" src="/img/MahoMangaDownloader/demo2/manga6.png" />'+
-'            <img class="img-manga-2" src="/img/MahoMangaDownloader/demo2/manga7.png" />'+
-'        </div>'+
-'    </div>'+
-'    <div class="detail">'+
-'        <h2>下載器說明</h2>'+
-'        <div class="support-websites">'+
-'            <h3>支援網站</h3>'+
-'            <ul class=\'support-list\'>'+
-'                <li><a class="link" target="_blank" href="http://www.dm5.com/">http://www.dm5.com/</a></li>'+
-'                <li><a class="link" target="_blank" href="http://www.manhuagui.com/">http://www.manhuagui.com/</a></li>'+
-'                <li><a class="link" target="_blank" href="http://www.huhudm.com/">http://www.huhudm.com/</a></li>'+
-'                <li><a class="link" target="_blank" href="http://99.hhxxee.com/">http://99.hhxxee.com/</a></li>'+
-'                <li><a class="link" target="_blank" href="http://www.comicbus.com/">http://www.comicbus.com/</a></li>'+
-'				<li><a class="link" target="_blank" href="https://www.manhuadui.com/">https://www.manhuadui.com/</a></li>'+
-'				<li><a class="link" target="_blank" href="https://www.comicun.com/">https://www.comicun.com/</a></li>'+
-'                <li><a class="link" target="_blank" href="https://e-hentai.org/">https://e-hentai.org/</a></li>'+
-'                <li><a class="link" target="_blank" href="https://exhentai.org/">https://exhentai.org/</a></li>'+
-'                <li><a class="link" target="_blank" href="https://nhentai.net/">https://nhentai.net/</a></li>'+
-'				<li><a class="link" target="_blank" href="https://www.wnacg.org/">https://www.wnacg.org/</a></li>'+
-'				<li><a class="link" target="_blank" href="https://hitomi.la/">https://hitomi.la/</a></li>'+
-'				<li><a class="link" target="_blank" href="https://hentai-comic.com/">https://hentai-comic.com/</a></li>'+
-'            </ul>'+
-'        </div>'+
-'        <div class="environment">'+
-'            <h3>環境需求</h3>'+
-'            <ol class=\'environment-list\'>'+
-'                <li><a class="link" target="_blank" href="https://www.microsoft.com/zh-tw/download/details.aspx?id=42642">.Net framework 4.5.2或以上的版本</a></li>'+
-'                <li><a class="link" target="_blank" href="https://www.microsoft.com/zh-TW/download/details.aspx?id=40784">Visual C++ 2013</a></li>'+
-'            </ol>'+
-'        </div>'+
-'        <div class="founction">'+
-'            <h3>功能說明</h3>'+
-'            <ol class=\'founction-list\'>'+
-'                <li>可批次下載。</li>'+
-'                <li>多列表管理下載清單。</li>'+
-'                <li>支援多執行緒下載(Max thread = 3, 請多珍惜各個資源網站)。</li>'+
-'                <li>支援搜尋功能, 搜尋過濾的關鍵字以及類別選單可在選單->設置的地方修改，預設為只輸出日文,中文,英文資源，分隔關鍵字為 \'|\' 字元, 另外也支援直接貼上網頁搜尋的Url網址。</li>'+
-'                <li>支援追蹤功能, 可將網址/關鍵字加入追蹤清單, 之後可掃描目前尚未下載的漫畫。</li>'+
-'                <li>語系: 中文/英文。</li>'+
-'                <li>要下載EX熊貓的資源或是本身有付費帳號的用戶,必須先設置一次Cookie(選單->設置->ehentai->Cookie),產生Cookie檔後即可下載EX or 受限制的資源, 之後皆不需重新設置。</li>'+
-'                <li>支援版本檢查 & 線上更新。</li>'+
-'            </ol>'+
-'        </div>'+
-'        <div class="notice">'+
-'            <h3>注意事項</h3>'+
-'            <ol class=\'notice-list\'>'+
-'                <li>e-hentai/exhentai有流量限制, 且下載過大會封鎖IP, 如果是網路環境為固定IP的話請加大delay時間。</li>'+
-'                <li>e-hentai/exhentai的快速搜尋只會Parse英文檔名, 除非有特定需求否則建議disable快速搜尋。</li>'+
-'                <li>在啟用重啟新IP功能時, 下載器會在被ban ip or 流量超過時自動換IP, 換IP過程電腦網路會暫時斷線, 並且只有網路環境為浮動IP的情況才有用。</li>'+
-'                <li>針對6.0以後的版本, 使用者資料(漫畫清單, 參數設置, Cookie檔案等等)皆會放在User_Data資料夾內, 更新下載器版本時User_Data內的檔案皆可銜接使用。</li>'+
-'            </ol>'+
-'        </div>'+
-'        <div class="reference">'+
-'            <h3>參考 & 使用套件</h3>'+
-'            <ul class=\'reference-list\'>'+
-'                <li><a class="link" target="_blank" href="https://github.com/cefsharp/cefsharp">CefSharp</a> - BSD LICNESE</li>'+
-'                <li><a class="link" target="_blank" href="https://github.com/zzzprojects">HtmlAgilityPack</a> - MIT LICNESE</li>'+
-'                <li><a class="link" target="_blank" href="https://github.com/jawa-the-hutt/lz-string-csharp">lz-string-csharp</a> - The Unlicense</li>'+
-'                <li><a class="link" target="_blank" href="https://github.com/imazen/libwebp-net">Imazen.WebP</a> - MIT LICNESE</li>'+
-'                <li><a class="link" target="_blank" href="http://logging.apache.org/log4net/">log4Net</a> - Apache License Version 2.0</li>'+
-'                <li><a class="link" target="_blank" href="http://www.newtonsoft.com/json">newtonsoft.Json</a> - MIT LICNESE</li>'+
-'                <li><a class="link" target="_blank" href="https://www.codeproject.com/Articles/117021/How-to-Create-ProgressBar-Column-in-DataGridView">DataGridViewProgressBarCell</a> - CPOL LICENSE</li>'+
-'            </ul>'+
-'        </div>'+
-'        <div class="disclaimer">'+
-'            <h3>免責聲明</h3>'+
-'            <h4>MahoMangaDownloader僅作為學術研究使用,禁止利用本程式行非法用途。</h4>'+
-'        </div>  '+
-'    </div>'+
-'</div>';
+var myvar = '<link rel="stylesheet" type="text/css" href="css/mahomangadownloader.css">' +
+    '<div class=\'main-MahoMangaDownloader\'>' +
+    '    <div class="demo-img">' +
+    '        <img src="/img/MahoMangaDownloader/MahoMangaDownloader_960x566.png"/>' +
+    '    </div>' +
+    '    <div class="description">' +
+    '        <p> MahoMangaDownloader是一款支援多個漫畫網站的漫畫下載器, 提供使用者下載各個網站的漫畫資源, 方便使用者試看漫畫。 MahoMangaDownloader支援以下功能:<br></p>' +
+    '            <ul class=\'description-function-list\'>' +
+    '                <li>MahoMangaDownloader支援排程 & 多執行緒下載, 方便使用者在電腦空餘時間下載大量漫畫。</li>' +
+    '                <li>多列表管理功能可讓使用者建立各類清單管理下載。</li>' +
+    '                <li>搜尋功能讓使用者可以在資源網站搜尋作品或作者名, 直接取得相關聯的漫畫列表。</li>' +
+    '                <li>追蹤功能可讓使用者建立追蹤清單, 只要執行掃描, 下載器就會去設定好的網址找出新更新或尚未下載的漫畫, 並自動加到設定好的下載清單。</li>' +
+    '                <li>剪貼簿偵測功能可改善大量複製漫畫網址時的程序步驟。</li>' +
+    '                <li>支援中英語系。</li>' +
+    '                <li>線上自動更新功能。</li>' +
+    '            </ul><br><br>' +
+    '        <p> 針對特定網站, 也提供下列功能:<br></p>' +
+    '            <ul class=\'description-function-list\'>' +
+    '                <li>針對某些只限內部會員漫畫網站, MahoMangaDownloader也提供使用者設定帳密或cookie的機制支援下載。</li>' +
+    '                <li>提供下載未壓縮過的原尺寸圖檔的功能。</li>' +
+    '                <li>某些網站會針對下載器的行為進行IP封鎖, 針對這類網站MahoMangaDownloader也提供流量限制偵測以及自動換IP的功能(限動態網路環境下), 讓下載能繼續順利進行。</li>' +
+    '            </ul><br><br>' +
+    '        <p>希望MahoMangaDownloader下載器能幫助使用者改善線上漫畫的閱覽體驗, 如果試看的漫畫您非常喜歡, 也請麻煩購買正版支持原作者, 讓作家們能繼續創造出下一部更棒的作品。</p>' +
+    '    </div>' +
+    '    <div class="manga-show">' +
+    '        <img class="img-manga" src="/img/MahoMangaDownloader/demo1/manga1.png" />' +
+    '        <img class="img-manga" src="/img/MahoMangaDownloader/demo1/manga2.png" />' +
+    '        <img class="img-manga" src="/img/MahoMangaDownloader/demo1/manga3.png" />' +
+    '        <img class="img-manga" src="/img/MahoMangaDownloader/demo1/manga4.png" />' +
+    '        <img class="img-manga" src="/img/MahoMangaDownloader/demo1/manga5.png" />' +
+    '        <img class="img-manga" src="/img/MahoMangaDownloader/demo1/manga6.png" />' +
+    '        <img class="img-manga" src="/img/MahoMangaDownloader/demo1/manga7.png" />' +
+    '    </div>' +
+    '    <div class="download-link">' +
+    '        <h2>下載 MahoMangaDownloader{Version} [解壓密碼:zmcx16]</h2>' +
+    '        <ul class=\'download-list\'>' +
+    '            <li><a class="link" target="_blank" href="{FilePathX64}"><img src="/img/MahoMangaDownloader/DownloadStartButton_30x30.png"><p>{FileNameX64} ({FileSizeX64} MB)</p></a></li>' +
+    '            <li><a class="link" target="_blank" href="{FilePathX86}"><img src="/img/MahoMangaDownloader/DownloadStartButton_30x30.png"><p>{FileNameX86} ({FileSizeX86} MB)</p></a></li>' +
+    '        </ul>' +
+    '    </div>' +
+    '    <div class="manga-show-2">' +
+    '        <div class="img-arrow-wrap">' +
+    '            <div class="img-arrow-wrap-2">' +
+    '                <img class="img-arrow" id="img-prev" src="/img/prev.png" />' +
+    '                <img class="img-arrow" id="img-next" src="/img/next.png" />' +
+    '            </div>' +
+    '        </div>' +
+    '        <div class="img-manga-2-wrap">' +
+    '            <img class="img-manga-2" src="/img/MahoMangaDownloader/demo2/manga1.png" />' +
+    '            <img class="img-manga-2" src="/img/MahoMangaDownloader/demo2/manga2.png" />' +
+    '            <img class="img-manga-2" src="/img/MahoMangaDownloader/demo2/manga3.png" />' +
+    '            <img class="img-manga-2" src="/img/MahoMangaDownloader/demo2/manga4.png" />' +
+    '            <img class="img-manga-2" src="/img/MahoMangaDownloader/demo2/manga5.png" />' +
+    '            <img class="img-manga-2" src="/img/MahoMangaDownloader/demo2/manga6.png" />' +
+    '            <img class="img-manga-2" src="/img/MahoMangaDownloader/demo2/manga7.png" />' +
+    '        </div>' +
+    '    </div>' +
+    '    <div class="detail">' +
+    '        <h2>下載器說明</h2>' +
+    '        <div class="support-websites">' +
+    '            <h3>支援網站</h3>' +
+    '            <ul class=\'support-list\'>' +
+    '                <li><a class="link" target="_blank" href="http://www.dm5.com/">http://www.dm5.com/</a></li>' +
+    '                <li><a class="link" target="_blank" href="http://www.manhuagui.com/">http://www.manhuagui.com/</a></li>' +
+    '                <li><a class="link" target="_blank" href="http://www.huhudm.com/">http://www.huhudm.com/</a></li>' +
+    '                <li><a class="link" target="_blank" href="http://99.hhxxee.com/">http://99.hhxxee.com/</a></li>' +
+    '                <li><a class="link" target="_blank" href="http://www.comicbus.com/">http://www.comicbus.com/</a></li>' +
+    '				<li><a class="link" target="_blank" href="https://www.manhuadui.com/">https://www.manhuadui.com/</a></li>' +
+    '				<li><a class="link" target="_blank" href="https://www.comicun.com/">https://www.comicun.com/</a></li>' +
+    '                <li><a class="link" target="_blank" href="https://e-hentai.org/">https://e-hentai.org/</a></li>' +
+    '                <li><a class="link" target="_blank" href="https://exhentai.org/">https://exhentai.org/</a></li>' +
+    '                <li><a class="link" target="_blank" href="https://nhentai.net/">https://nhentai.net/</a></li>' +
+    '				<li><a class="link" target="_blank" href="https://www.wnacg.org/">https://www.wnacg.org/</a></li>' +
+    '				<li><a class="link" target="_blank" href="https://hitomi.la/">https://hitomi.la/</a></li>' +
+    '                <li><a class="link" target="_blank" href="https://hentai-comic.com/">https://hentai-comic.com/</a></li>' +
+    '                <li><a class="link" target="_blank" href="https://manhua.dmzj.com/">https://manhua.dmzj.com/</a></li>' +
+    '                <li><a class="link" target="_blank" href="https://www.youma.org/">https://www.youma.org/</a></li>' +
+    '            </ul>' +
+    '        </div>' +
+    '        <div class="environment">' +
+    '            <h3>環境需求</h3>' +
+    '            <ol class=\'environment-list\'>' +
+    '                <li><a class="link" target="_blank" href="https://www.microsoft.com/zh-tw/download/details.aspx?id=42642">.Net framework 4.5.2或以上的版本</a></li>' +
+    '                <li><a class="link" target="_blank" href="https://www.microsoft.com/zh-TW/download/details.aspx?id=40784">Visual C++ 2013</a></li>' +
+    '            </ol>' +
+    '        </div>' +
+    '        <div class="founction">' +
+    '            <h3>功能說明</h3>' +
+    '            <ol class=\'founction-list\'>' +
+    '                <li>可批次下載。</li>' +
+    '                <li>多列表管理下載清單。</li>' +
+    '                <li>支援多執行緒下載(Max thread = 3, 請多珍惜各個資源網站)。</li>' +
+    '                <li>支援搜尋功能, 搜尋過濾的關鍵字以及類別選單可在選單->設置的地方修改，預設為只輸出日文,中文,英文資源，分隔關鍵字為 \'|\' 字元, 另外也支援直接貼上網頁搜尋的Url網址。</li>' +
+    '                <li>支援追蹤功能, 可將網址/關鍵字加入追蹤清單, 之後可掃描目前尚未下載的漫畫。</li>' +
+    '                <li>語系: 中文/英文。</li>' +
+    '                <li>要下載EX熊貓的資源或是本身有付費帳號的用戶,必須先設置一次Cookie(選單->設置->ehentai->Cookie),產生Cookie檔後即可下載EX or 受限制的資源, 之後皆不需重新設置。</li>' +
+    '                <li>支援版本檢查 & 線上更新。</li>' +
+    '            </ol>' +
+    '        </div>' +
+    '        <div class="notice">' +
+    '            <h3>注意事項</h3>' +
+    '            <ol class=\'notice-list\'>' +
+    '                <li>e-hentai/exhentai有流量限制, 且下載過大會封鎖IP, 如果是網路環境為固定IP的話請加大delay時間。</li>' +
+    '                <li>e-hentai/exhentai的快速搜尋只會Parse英文檔名, 除非有特定需求否則建議disable快速搜尋。</li>' +
+    '                <li>在啟用重啟新IP功能時, 下載器會在被ban ip or 流量超過時自動換IP, 換IP過程電腦網路會暫時斷線, 並且只有網路環境為浮動IP的情況才有用。</li>' +
+    '                <li>針對6.0以後的版本, 使用者資料(漫畫清單, 參數設置, Cookie檔案等等)皆會放在User_Data資料夾內, 更新下載器版本時User_Data內的檔案皆可銜接使用。</li>' +
+    '            </ol>' +
+    '        </div>' +
+    '        <div class="reference">' +
+    '            <h3>參考 & 使用套件</h3>' +
+    '            <ul class=\'reference-list\'>' +
+    '                <li><a class="link" target="_blank" href="https://github.com/cefsharp/cefsharp">CefSharp</a> - BSD LICNESE</li>' +
+    '                <li><a class="link" target="_blank" href="https://github.com/zzzprojects">HtmlAgilityPack</a> - MIT LICNESE</li>' +
+    '                <li><a class="link" target="_blank" href="https://github.com/jawa-the-hutt/lz-string-csharp">lz-string-csharp</a> - The Unlicense</li>' +
+    '                <li><a class="link" target="_blank" href="https://github.com/imazen/libwebp-net">Imazen.WebP</a> - MIT LICNESE</li>' +
+    '                <li><a class="link" target="_blank" href="http://logging.apache.org/log4net/">log4Net</a> - Apache License Version 2.0</li>' +
+    '                <li><a class="link" target="_blank" href="http://www.newtonsoft.com/json">newtonsoft.Json</a> - MIT LICNESE</li>' +
+    '                <li><a class="link" target="_blank" href="https://www.codeproject.com/Articles/117021/How-to-Create-ProgressBar-Column-in-DataGridView">DataGridViewProgressBarCell</a> - CPOL LICENSE</li>' +
+    '            </ul>' +
+    '        </div>' +
+    '        <div class="disclaimer">' +
+    '            <h3>免責聲明</h3>' +
+    '            <h4>MahoMangaDownloader僅作為學術研究使用,禁止利用本程式行非法用途。</h4>' +
+    '        </div>  ' +
+    '    </div>' +
+    '</div>';
 
 document.getElementById('main-plugin-wrap').innerHTML = myvar;
 
