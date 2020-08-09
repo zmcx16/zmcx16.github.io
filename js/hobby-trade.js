fb0317794fdf430c86e7b24f509de7bd58baed66
zmcx16 <zmcx16@gmail.com>
2020-08-09 16:46:26 +0800

    implement get trade-data

--- js/hobby-trade.js
+++ js/hobby-trade.js
@@ -5,7 +5,7 @@ var myvar =
 '        <img src="/img/hobby-trade/trade-kanban_960x435.png" />' +
 '    </div>' +
 '    <div class="hold-stocks-groups">' +
-'        <h1> 持有個股 </h1>' +
+'        <h1> 持有清單 </h1>' +
 '        <div class="hold-stocks content-block">' +
 '            <table id="hold-stocks-table">' +
 '                <thead>' +
@@ -24,6 +24,26 @@ var myvar =
 '            </table>' +
 '        </div>' +
 '    </div>' +
+'    <div class="star-stocks-groups">' +
+'        <h1> 關注清單 </h1>' +
+'        <div class="star-stocks content-block">' +
+'            <table id="star-stocks-table">' +
+'                <thead>' +
+'                    <th class="th-symbol">Symbol</th>' +
+'                    <th class="th-price">Price</th>' +
+'                    <th class="th-52w">52W</th>' +
+'                    <th class="th-sma20">SMA20</th>' +
+'                    <th class="th-sma50">SMA50</th>' +
+'                    <th class="th-sma200">SMA200</th>' +
+'                    <th class="th-rsi">RSI</th>' +
+'                    <th class="th-atr">ATR</th>' +
+'                    <th class="th-risk">Risk</th>' +
+'                </thead>' +
+'                <tbody id="star-stocks-tbody">' +
+'                </tbody>' +
+'            </table>' +
+'        </div>' +
+'    </div>' +
 '    <div class="data-source groups">' +
 '        <h1> 數據源 </h1>' +
 '        <div class="data-source content-block">' +
@@ -32,7 +52,6 @@ var myvar =
 '            <a class="link" target="_blank" href="https://finviz.com/">Finviz</a>' +
 '        </div>' +
 '    </div>' +
-'    <h1>網站建置中...</h1>' +
 '</div>' ;
 
 document.getElementById('main-plugin-wrap').innerHTML = myvar;
@@ -160,7 +179,7 @@ function buildTable(data){
     '        <div class="scan-list">' +
     '          <table class="scan-table">' +
     '            <thead>' +
-    '              <th class="th-type-name">Scan Strategy</th>' +
+    '              <th class="th-type-name">Scan Tactic</th>' +
     '              <th class="th-risk">Risk</th>' +
     '            </thead>' +    
     '            <tbody class="scan-tbody">';
@@ -182,60 +201,72 @@ function buildTable(data){
   return output;
 }
 
-$(document).ready(function () {
+function preprocessData(json_data, input_key)
+{
+  var output = []
 
-  $.getJSON("zmcx16_hobby-trade-data.json", function (json_data) {
+  json_data[input_key].forEach((symbol) => {
 
-    var hold_stocks = []
+    var base_info = {};
+    var scan_list = [];
 
-    json_data["hold_stock_list"].forEach((symbol) => {
-   
-      var base_info = {};
-      var scan_list = [];
+    json_data["data"].forEach((stock) => {
 
-      json_data["data"].forEach((stock) => {
-        
-        if (stock["symbol"] == symbol){
-          if (scan_list.length === 0){
-            base_info["Price"] = stock["baseinfo"]["Price"];
-            base_info["52W Range"] = stock["baseinfo"]["52W Range"];
-            base_info["SMA20"] = stock["baseinfo"]["SMA20"];
-            base_info["SMA50"] = stock["baseinfo"]["SMA50"];
-            base_info["SMA200"] = stock["baseinfo"]["SMA200"];
-            base_info["RSI (14)"] = stock["baseinfo"]["RSI (14)"];
-            base_info["ATR"] = stock["baseinfo"]["ATR"];
-            base_info["Beta"] = stock["baseinfo"]["Beta"];
-            base_info["Market Cap"] = stock["baseinfo"]["Market Cap"];
-            base_info["Dividend %"] = stock["baseinfo"]["Dividend %"];
-            base_info["P/E"] = stock["baseinfo"]["P/E"];
-            base_info["Forward P/E"] = stock["baseinfo"]["Forward P/E"];
-            base_info["PEG"] = stock["baseinfo"]["PEG"];
-            base_info["Debt/Eq"] = stock["baseinfo"]["Debt/Eq"];
-            base_info["LT Debt/Eq"] = stock["baseinfo"]["LT Debt/Eq"];
-            base_info["EPS (ttm)"] = stock["baseinfo"]["EPS (ttm)"];
-            base_info["ROA"] = stock["baseinfo"]["ROA"];
-            base_info["ROE"] = stock["baseinfo"]["ROE"];
-            base_info["ROI"] = stock["baseinfo"]["ROI"];
-            base_info["Gross Margin"] = stock["baseinfo"]["Gross Margin"];
-            base_info["Oper. Margin"] = stock["baseinfo"]["Oper. Margin"];
-            base_info["Profit Margin"] = stock["baseinfo"]["Profit Margin"];
-          }
-
-          var report = {
-            "type": stock["type"],
-            "report": stock["report"]
-          };
-
-          scan_list.push(report);
+      if (stock["symbol"] == symbol) {
+        if (scan_list.length === 0) {
+          base_info["Price"] = stock["baseinfo"]["Price"];
+          base_info["52W Range"] = stock["baseinfo"]["52W Range"];
+          base_info["SMA20"] = stock["baseinfo"]["SMA20"];
+          base_info["SMA50"] = stock["baseinfo"]["SMA50"];
+          base_info["SMA200"] = stock["baseinfo"]["SMA200"];
+          base_info["RSI (14)"] = stock["baseinfo"]["RSI (14)"];
+          base_info["ATR"] = stock["baseinfo"]["ATR"];
+          base_info["Beta"] = stock["baseinfo"]["Beta"];
+          base_info["Market Cap"] = stock["baseinfo"]["Market Cap"];
+          base_info["Dividend %"] = stock["baseinfo"]["Dividend %"];
+          base_info["P/E"] = stock["baseinfo"]["P/E"];
+          base_info["Forward P/E"] = stock["baseinfo"]["Forward P/E"];
+          base_info["PEG"] = stock["baseinfo"]["PEG"];
+          base_info["Debt/Eq"] = stock["baseinfo"]["Debt/Eq"];
+          base_info["LT Debt/Eq"] = stock["baseinfo"]["LT Debt/Eq"];
+          base_info["EPS (ttm)"] = stock["baseinfo"]["EPS (ttm)"];
+          base_info["ROA"] = stock["baseinfo"]["ROA"];
+          base_info["ROE"] = stock["baseinfo"]["ROE"];
+          base_info["ROI"] = stock["baseinfo"]["ROI"];
+          base_info["Gross Margin"] = stock["baseinfo"]["Gross Margin"];
+          base_info["Oper. Margin"] = stock["baseinfo"]["Oper. Margin"];
+          base_info["Profit Margin"] = stock["baseinfo"]["Profit Margin"];
         }
 
-      });
+        var report = {
+          "type": stock["type"],
+          "report": stock["report"]
+        };
 
-      hold_stocks.push({ "symbol": symbol, "base_info": base_info, "scan_list": scan_list});
+        scan_list.push(report);
+      }
 
     });
 
+    output.push({ "symbol": symbol, "base_info": base_info, "scan_list": scan_list });
+
+  });
+
+  return output;
+}
+
+$(document).ready(function () {
+
+  // disable cache
+  // $.ajaxSetup({ cache: false });
+
+  $.getJSON("zmcx16_hobby-trade-data.json", function (json_data) {
+
+    var hold_stocks = preprocessData(json_data, "hold_stock_list");
+    var star_stocks = preprocessData(json_data, "star_stock_list");
+
     $("#hold-stocks-tbody")[0].innerHTML = buildTable(hold_stocks);
+    $("#star-stocks-tbody")[0].innerHTML = buildTable(star_stocks);
 
     $(".tr-stock.main, .tr-scan.main").click(function (e) {
       var second = $(this).next();
