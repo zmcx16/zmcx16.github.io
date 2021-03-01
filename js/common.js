cdb808c4c095c4692c9e94347752826e145bb659
zmcx16 <zmcx16@gmail.com>
2021-03-01 21:36:24 +0800

    implement factor intersectional formula

--- js/common.js
+++ js/common.js
@@ -163,3 +163,11 @@ class SwitchImgs {
         clearInterval(this.timeId);
     }
 }
+
+function convertKMBT(n, precision) {
+  if (n < 1e3) return n;
+  if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(precision) + "K";
+  if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(precision) + "M";
+  if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(precision) + "B";
+  if (n >= 1e12) return +(n / 1e12).toFixed(precision) + "T";
+}
