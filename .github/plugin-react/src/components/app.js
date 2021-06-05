1e650dc23c744ac53a299c66bb363acd7bbc4c10
zmcx16 <zmcx16@gmail.com>
2021-06-06 02:32:04 +0800

    add plugin-react

---
+++ .github/plugin-react/src/components/app.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import commonStyle from './common.module.scss'
+
+const App = () => {
+  return (
+    <>
+      <div className={commonStyle.defaultFont}>
+        test
+      </div>
+    </>
+  )
+}
+
+export default App
