3c85b43968c15f59cccccc79bc5c0d8e41c33d5a
zmcx16 <zmcx16@gmail.com>
2021-06-14 22:16:29 +0800

    implement monitor page

---
+++ .github/plugin-react/src/components/monitor.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import commonStyle from './common.module.scss'
+
+const Monitor = () => {
+  return (
+    <>
+      <div className={commonStyle.defaultFont}>
+        網站建置中...
+      </div>
+    </>
+  )
+}
+
+export default Monitor
