b25a08b6837edd526d136e77b855d9de5e9166a1
zmcx16 <zmcx16@gmail.com>
2021-06-19 01:50:18 +0800

    implement monitor page

--- .github/plugin-react/src/components/monitor.js
+++ .github/plugin-react/src/components/monitor.js
@@ -5,7 +5,11 @@ import shortid from 'shortid'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Button from '@material-ui/core/Button'
 import SearchIcon from '@material-ui/icons/Search'
+import useFetch from 'use-http'
+
 import { IOSSwitch } from './iOSSwitch'
+import { MonitorConfig } from '../common/config'
+import MonitorTable from './monitorTable'
 
 import commonStyle from './common.module.scss'
 import monitorStyle from './monitor.module.scss'
@@ -14,25 +18,59 @@ const Monitor = () => {
 
   const apiTokenRef = useRef("None")
   const refreshTimeMinutesRef = useRef(5)
-  const monitorUrlRef = useRef("https://")
 
   const [monitoring, setMonitoring] = useState(false) 
+
+  const { get, response, error } = useFetch({ cachePolicy: 'no-cache' })
+
+  const refreshMonitorData = async (monitor_index) => {
+    const resp_data = await get('/test_data.json')
+    if (response.ok) {
+      monitorTablesRef.current[monitor_index].current.setTable(resp_data.data)
+    }
+    else {
+      console.error(error)
+    }
+  }
+
+  const monitorTablesRef = useRef([])
+  MonitorConfig.monitor_data.forEach((value, index) => {
+    monitorTablesRef.current[index] = createRef()
+    monitorTablesRef.current[index].current = {
+      setTable: null
+    }
+  })
+
+  useEffect(() => {
+    // componentDidMount is here!
+    // componentDidUpdate is here!
+    if (monitoring) {
+      MonitorConfig.monitor_data.forEach((value, index) => {
+        refreshMonitorData(index)
+      })
+    }
+
+    return () => {
+      // componentWillUnmount is here!
+    }
+  }, [monitoring])
+
 
   return (
     <div className={commonStyle.defaultFont + ' ' + monitorStyle.container}>
       <div className={monitorStyle.controlPannel}>
         <Grid container spacing={3}>
-          <Grid item xs={5}>
+          <Grid item xs={8}>
             <form noValidate autoComplete="off" key={shortid.generate()}>
               <TextField className={monitorStyle.valueText} label={'API Token'} variant="outlined" defaultValue={'None'} size="small" inputRef={apiTokenRef} />
             </form>
           </Grid>
-          <Grid item xs={3}>
+          <Grid item xs={2}>
             <form noValidate autoComplete="off" key={shortid.generate()}>
               <TextField className={monitorStyle.valueText} label={'Refresh Time (min)'} variant="outlined" defaultValue={'5'} size="small" inputRef={refreshTimeMinutesRef} />
             </form>
           </Grid>
-          <Grid container item xs={4} justify="flex-end">
+          <Grid container item xs={2} justify="flex-end">
             <div className={monitorStyle.ControlToggleContainer} >
               <FormControlLabel
                 className={monitorStyle.ControlToggle}
@@ -47,18 +85,12 @@ const Monitor = () => {
               />
             </div>
           </Grid>
-          <Grid item xs={10}>
-            <form noValidate autoComplete="off" key={shortid.generate()}>
-              <TextField className={monitorStyle.valueText} label={'Monitor Url'} variant="outlined" defaultValue={'None'} size="small" inputRef={monitorUrlRef} />
-            </form>
-          </Grid>
-          <Grid item xs={2}>
-            <div>
-              <Button className={monitorStyle.monitorBtn}  variant="contained" color="primary" startIcon={<SearchIcon />} onClick={() => {
-              }}>{'Add Url'}</Button>
-            </div>
-          </Grid>
         </Grid>
+      </div>
+      <div>
+        {monitorTablesRef.current.map((value, index) => {
+          return <MonitorTable key={shortid.generate()} MonitorTableRef={monitorTablesRef.current[index]} />
+        })}
       </div>
     </div>
   )
