9bd2ba46338c21d0eac1ef16bbfe7d33da8947d1
zmcx16 <zmcx16@gmail.com>
2021-06-20 04:43:40 +0800

    implement monitor

--- .github/plugin-react/gatsby-node.js
+++ .github/plugin-react/gatsby-node.js
@@ -1,8 +1,16 @@
-const fs = require('fs-extra');
-const path = require('path');
-const rimraf = require("rimraf");
+const fs = require('fs-extra')
+const path = require('path')
+const rimraf = require("rimraf")
+
+const config = require('./src/common/config')
 
 exports.onPostBuild = function () {
-  rimraf.sync(path.join(__dirname, '..', '..', 'plugin-react'));
-  fs.copySync(path.join(__dirname, 'public'), path.join(__dirname, '..', '..', 'plugin-react'));
-};
+  let public_src_path = path.join(__dirname, 'public')
+  let public_output_path = path.join(__dirname, '..', '..', 'plugin-react')
+
+  // dump config.js to config.json
+  fs.writeFileSync(path.join(public_src_path, 'config.json'), JSON.stringify(config.getMonitorConfig()))
+
+  rimraf.sync(public_output_path)
+  fs.copySync(public_src_path, public_output_path)
+}
