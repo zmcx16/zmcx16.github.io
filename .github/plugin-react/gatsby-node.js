2d01dc500692669fdf9398bc68d7e7ada7704a32
zmcx16 <zmcx16@gmail.com>
2021-06-20 23:02:51 +0800

    enable build-react-plugin

--- .github/plugin-react/gatsby-node.js
+++ .github/plugin-react/gatsby-node.js
@@ -8,8 +8,8 @@ exports.onPostBuild = function () {
   let public_src_path = path.join(__dirname, 'public')
   let public_output_path = path.join(__dirname, '..', '..', 'plugin-react')
 
-  // dump config.js to config.json
-  fs.writeFileSync(path.join(public_src_path, 'config.json'), JSON.stringify(config.getMonitorConfig()))
+  // dump config.js monitor_config to monitor_config.json
+  fs.writeFileSync(path.join(public_src_path, 'monitor_config.json'), JSON.stringify(config.getMonitorConfig()))
 
   rimraf.sync(public_output_path)
   fs.copySync(public_src_path, public_output_path)
