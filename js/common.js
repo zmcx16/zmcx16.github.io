18f67bbab09cb5b11f827e4c1d0260a5e963f12a
zmcx16 <zmcx16@gmail.com>
2021-06-21 01:21:40 +0800

    fix iframe height can't auto adjust

--- js/common.js
+++ js/common.js
@@ -206,9 +206,9 @@ function SetIFrameHeight() {
     var iframeid = document.getElementById("mainframe");
     if (document.getElementById) {
         if (iframeid && !window.opera) {
-            if (iframeid.contentDocument && iframeid.contentDocument.body.offsetHeight) {
+            if (iframeid.contentDocument && iframeid.contentDocument.body.offsetHeight && iframeid.height != iframeid.contentDocument.body.offsetHeight) {
                 iframeid.height = iframeid.contentDocument.body.offsetHeight;
-            } else if (iframeid.Document && iframeid.Document.body.scrollHeight) {
+            } else if (iframeid.Document && iframeid.Document.body.scrollHeight && iframeid.height != iframeid.Document.body.scrollHeight) {
                 iframeid.height = iframeid.Document.body.scrollHeight;
             }
         }
