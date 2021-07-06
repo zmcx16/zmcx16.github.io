9e48b334802b8e349d834551503112be248688c0
zmcx16 <zmcx16@gmail.com>
2021-07-07 00:18:24 +0800

    add market_config

--- .github/plugin-react/gatsby-node.js
+++ .github/plugin-react/gatsby-node.js
@@ -10,6 +10,7 @@ exports.onPostBuild = function () {
 
   // dump config.js monitor_config to monitor_config.json
   fs.writeFileSync(path.join(public_src_path, 'monitor_config.json'), JSON.stringify(config.getMonitorConfig()))
+  fs.writeFileSync(path.join(public_src_path, 'market_config.json'), JSON.stringify(config.getMarketConfig()))
 
   rimraf.sync(public_output_path)
   fs.copySync(public_src_path, public_output_path)
