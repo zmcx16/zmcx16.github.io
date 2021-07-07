b53c0ea6f86e4265b7e82fb7b1e633ef10a174c1
zmcx16 <zmcx16@gmail.com>
2021-07-07 23:36:36 +0800

    implement correlation matrix

--- .github/plugin-react/src/common/def.js
+++ .github/plugin-react/src/common/def.js
@@ -3,3 +3,6 @@ export const MonitorURL = 'https://stockminehunterfuncmonitor0.azurewebsites.net
 // cookie
 export const COOKIE_KEY_API_TOKEN = 'api_token'
 export const COOKIE_KEY_REFRESH_TIME = 'refresh_time'
+
+// const
+export const rollingCorrelationWindowDays = 90
--- .github/plugin-react/src/pages/monitor.js
+++ .github/plugin-react/src/pages/monitor.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StylesProvider } from '@material-ui/core/styles'
 
-import Monitor from '../components/monitor'
+import Monitor from '../components/monitor/monitor'
 
 const MonitorPage = () => {
   return (
