5ef980969a04a4e6e43ef23aeb3f1d77e3b2b484
zmcx16 <zmcx16@gmail.com>
2022-01-25 21:32:51 +0800

    add build_option_valuation

---
+++ .github/script/build_option_valuation.py
@@ -0,0 +1,37 @@
+import os
+import pathlib
+import json
+import shutil
+import logging
+import traceback
+
+
+def get_option_data(config, option_folder_path):
+    try:
+        hold_list = config["hold_stock_list"]
+        hold_list_str = ",".join(hold_list)
+        os.system("python ./Norn-Finance-API-Server/option_cron_job.py -i " + hold_list_str)
+        shutil.copytree('./Norn-Finance-API-Server/output', option_folder_path, dirs_exist_ok=True)
+
+    except Exception:
+        logging.error(traceback.format_exc())
+
+    logging.info('get_option_data done')
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    root = pathlib.Path(__file__).parent.resolve()
+    plugin_react_folder_path = root / ".." / ".." / "plugin-react"
+
+    input_path = root / '..' / 'trade-data' / 'trade-data.json'
+    option_folder_path = plugin_react_folder_path / 'option-valuation'
+    if not os.path.exists(option_folder_path):
+        os.makedirs(option_folder_path)
+
+    with open(input_path, 'r', encoding='utf-8') as f:
+        config = json.loads(f.read())
+        get_option_data(config, option_folder_path)
+
+    logging.info('all task done')
+
