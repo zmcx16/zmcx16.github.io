17861589428d10bd5d9879e81c00128011cbf6b1
zmcx16 <zmcx16@gmail.com>
2022-03-07 23:49:18 +0800

    add MC chart on option page

--- .github/plugin-react/src/common/utils.js
+++ .github/plugin-react/src/common/utils.js
@@ -52,6 +52,17 @@ export const tryConvertFloat = (v) => {
   }
 }
 
+export function workdayCount(start,end) {
+  var first = start.clone().endOf('week'); // end of first week
+  var last = end.clone().startOf('week'); // start of last week
+  var days = last.diff(first,'days') * 5 / 7; // this will always multiply of 7
+  var wfirst = first.day() - start.day(); // check first week
+  if(start.day() == 0) --wfirst; // -1 if start with sunday 
+  var wlast = end.day() - last.day(); // check last week
+  if(end.day() == 6) --wlast; // -1 if end with saturday
+  return wfirst + Math.floor(days) + wlast; // get the total
+} //              ^ EDIT: if days count less than 7 so no decimal point
+
 // ref: https://www.colorhexa.com/ff0000
 export function getRedLevel (val){
   if (val >= 6.0 / 7.0) {
