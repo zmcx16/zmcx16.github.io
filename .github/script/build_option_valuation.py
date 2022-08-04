fc4c35b43d1b439d854baa01049d7f69874f3469
zmcx16 <zmcx16@gmail.com>
2022-08-05 06:49:48 +0800

    replace os.system with os.popen

--- .github/script/build_option_valuation.py
+++ .github/script/build_option_valuation.py
@@ -12,7 +12,10 @@ def get_option_data(config, option_star_folder_path, option_hold_folder_path, lo
         hold_stock_list = config["hold_stock_list"]
         star_options = config["star_option_list"]
         star_list_str = ",".join(hold_stock_list+star_options)
-        os.system("python ./.github/script/Norn-Finance-API-Server/option_cron_job.py -l " + log_level + " -i " + star_list_str)
+        # os.system("python ./.github/script/Norn-Finance-API-Server/option_cron_job.py -l " + log_level + " -i " + star_list_str)
+        out = os.popen("python ./.github/script/Norn-Finance-API-Server/option_cron_job.py -l " + log_level + " -i " + star_list_str).read()
+        logging.info(out)
+
         shutil.copytree('./.github/script/Norn-Finance-API-Server/output', option_star_folder_path, dirs_exist_ok=True)
 
         shutil.rmtree('./.github/script/Norn-Finance-API-Server/output')
@@ -22,7 +25,9 @@ def get_option_data(config, option_star_folder_path, option_hold_folder_path, lo
         for sc in hold_options:
             specific_contracts.append(sc["symbol"] + "_" + sc["type"] + "_" + sc["expiry"] + "_" + str(sc["strike"]))
 
-        os.system("python ./.github/script/Norn-Finance-API-Server/option_cron_job.py -l " + log_level + " -s " + ",".join(specific_contracts))
+        # os.system("python ./.github/script/Norn-Finance-API-Server/option_cron_job.py -l " + log_level + " -s " + ",".join(specific_contracts))
+        out = os.popen("python ./.github/script/Norn-Finance-API-Server/option_cron_job.py -l " + log_level + " -s " + ",".join(specific_contracts))
+        logging.info(out)
         shutil.copytree('./.github/script/Norn-Finance-API-Server/output', option_hold_folder_path, dirs_exist_ok=True)
 
     except Exception:
