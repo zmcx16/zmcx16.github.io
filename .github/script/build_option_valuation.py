aa1cf613e667d83a0a51b759530256ffb64d989e
zmcx16 <zmcx16@gmail.com>
2022-08-06 23:30:33 +0800

    encrypt option output

--- .github/script/build_option_valuation.py
+++ .github/script/build_option_valuation.py
@@ -4,26 +4,90 @@ import json
 import shutil
 import logging
 import argparse
+import requests
 import traceback
+import base64
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad,unpad
+from Crypto.Random import get_random_bytes
 
 
-def get_option_data(config, option_star_folder_path, option_hold_folder_path, log_level, data_source):
+private_config_url = os.environ.get("CONFIG_URL", "")
+encry_pass = os.environ.get("ENCRY_PASS", "")
+
+
+def encrypt(raw, key):
+    raw = pad(raw.encode(), 16)
+    cipher = AES.new(key.encode('utf-8'), AES.MODE_ECB)
+    return base64.b64encode(cipher.encrypt(raw))
+
+
+def decrypt(enc, key):
+    enc = base64.b64decode(enc)
+    cipher = AES.new(key.encode('utf-8'), AES.MODE_ECB)
+    return unpad(cipher.decrypt(enc), 16)
+
+
+def encry_all_files(folder_path, endswith):
+    for file in os.listdir(folder_path):
+        if file.endswith(endswith):
+            file_path = os.path.join(folder_path, file)
+            with open(file_path, "r+") as f:
+                output = encrypt(f.read(), encry_pass)
+                f.seek(0)
+                f.write(output.decode("utf-8", "ignore"))
+                f.truncate()
+
+
+def send_request(url):
     try:
-        hold_stock_list = config["hold_stock_list"]
-        star_options = config["star_option_list"]
+        res = requests.get(url)
+        res.raise_for_status()
+    except Exception as ex:
+        logging.error(traceback.format_exc())
+        return -1, ex
+
+    return 0, res.text
+
+
+def get_option_data(public_config, private_config, option_folder_path, log_level, data_source):
+    try:
+        nfas_output_path = './.github/script/Norn-Finance-API-Server/output'
+
+        option_config = {
+            "hold_stock_list": public_config["hold_stock_list"],
+            "hold_option_list": public_config["hold_option_list"],
+            "star_option_list": private_config["star_option_list"]
+        }
+
+        option_star_folder_path = option_folder_path / 'star'
+        if not os.path.exists(option_star_folder_path):
+            os.makedirs(option_star_folder_path)
+
+        option_hold_folder_path = option_folder_path / 'hold'
+        if not os.path.exists(option_hold_folder_path):
+            os.makedirs(option_hold_folder_path)
+
+        hold_stock_list = option_config["hold_stock_list"]
+        star_options = option_config["star_option_list"]
         star_list_str = ",".join(hold_stock_list+star_options)
         os.system("python ./.github/script/Norn-Finance-API-Server/option_cron_job.py -l " + log_level + " -d " + data_source + " -i " + star_list_str)
-        shutil.copytree('./.github/script/Norn-Finance-API-Server/output', option_star_folder_path, dirs_exist_ok=True)
+        shutil.copytree(nfas_output_path, option_star_folder_path, dirs_exist_ok=True)
 
         shutil.rmtree('./.github/script/Norn-Finance-API-Server/output')
 
-        hold_options = config["hold_option_list"]
+        hold_options = option_config["hold_option_list"]
         specific_contracts = []
         for sc in hold_options:
             specific_contracts.append(sc["symbol"] + "_" + sc["type"] + "_" + sc["expiry"] + "_" + str(sc["strike"]))
 
         os.system("python ./.github/script/Norn-Finance-API-Server/option_cron_job.py -l " + log_level + " -d " + data_source + " -s " + ",".join(specific_contracts))
-        shutil.copytree('./.github/script/Norn-Finance-API-Server/output', option_hold_folder_path, dirs_exist_ok=True)
+        shutil.copytree(nfas_output_path, option_hold_folder_path, dirs_exist_ok=True)
+
+        with open(option_folder_path / 'config.json', 'w', encoding='utf-8') as f:
+            f.write(json.dumps(option_config, separators=(',', ':')))
+
+        encry_all_files(option_folder_path, ".json")
 
     except Exception:
         logging.error(traceback.format_exc())
@@ -43,17 +107,14 @@ if __name__ == "__main__":
     plugin_react_folder_path = root / ".." / ".." / "plugin-react"
 
     input_path = root / ".." / ".." / 'trade-data.json'
-    option_star_folder_path = plugin_react_folder_path / 'option-valuation' / 'star'
-    if not os.path.exists(option_star_folder_path):
-        os.makedirs(option_star_folder_path)
-
-    option_hold_folder_path = plugin_react_folder_path / 'option-valuation' / 'hold'
-    if not os.path.exists(option_hold_folder_path):
-        os.makedirs(option_hold_folder_path)
-
     with open(input_path, 'r', encoding='utf-8') as f:
-        config = json.loads(f.read())
-        get_option_data(config, option_star_folder_path, option_hold_folder_path, args.log_level, args.data_source)
+        public_config = json.loads(f.read())
+        ret, resp = send_request(private_config_url)
+        if ret != 0:
+            logging.error('get private config failed')
+            exit(-1)
+
+        private_config = json.loads(resp)
+        get_option_data(public_config, private_config, plugin_react_folder_path / 'option-valuation', args.log_level, args.data_source)
 
     logging.info('all task done')
-
