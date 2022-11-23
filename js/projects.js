910877e44037343802e641e7ec90d2d7079e3d45
zmcx16 <zmcx16@gmail.com>
2022-11-23 23:04:32 +0800

    add Norn-Finance-Backtesting

--- js/projects.js
+++ js/projects.js
@@ -54,38 +54,46 @@ oss_projects['PhotoMosaic-Artifact'] = {
   background: 'PhotoMosaic-Artifact_885x450.jpg',
   msg: 'Photo Mosaic Artifact...中二病?'
 };
-oss_projects['stock-forecast'] = {
-  blog_post: 'https://blog.zmcx16.moe/search/label/stock-forecast',
-  featured: true,
-  position: 8,
-  background: 'stock-forecast_885x450.png',
-  msg: 'エンディングが、見えた'
-};
 oss_projects['Norn-Finance-API-Server'] = {
   blog_post: 'https://blog.zmcx16.moe/search/label/Norn-Finance-API-Server',
   online_website: 'https://norn-stockscreener.zmcx16.moe/options/',
   featured: true,
-  position: 9,
+  position: 8,
   background: 'Norn-Finance-API-Server_885x450.png',
   msg: '選擇權估值模型...好深奧...'
+};
+oss_projects['Norn-Finance-Backtesting'] = {
+  blog_post: 'https://github.com/zmcx16/Norn-Finance-Backtesting',
+  featured: true,
+  position: 9,
+  background: 'Norn-Finance-Backtesting_885x450.png',
+  msg: 'スカイクラッドの観測者'
+};
+
+oss_projects['stock-forecast'] = {
+  blog_post: 'https://blog.zmcx16.moe/search/label/stock-forecast',
+  featured: true,
+  position: 10,
+  background: 'stock-forecast_885x450.png',
+  msg: 'エンディングが、見えた'
 };
 oss_projects['TW-Stock Lottery Secretary'] = {
   blog_post: 'https://blog.zmcx16.moe/search/label/FB%E7%B2%89%E7%B5%B2%E5%B0%88%E9%A0%81%E6%A9%9F%E5%99%A8%E4%BA%BA%E9%96%8B%E7%99%BC%20-%20%E8%82%A1%E7%A5%A8%E6%8A%BD%E7%B1%A4%E5%B0%8F%E7%A7%98%E6%9B%B8',
   featured: true,
-  position: 10,
+  position: 11,
   background: 'TWStockLotterySecretary_885x450.jpg',
   msg: '歡迎加入追蹤+訂閱喔~'
 };
 oss_projects['twitter-images-collector'] = {
   blog_post: 'https://blog.zmcx16.moe/search/label/twitter-images-collector',
   featured: true,
-  position: 11,
+  position: 12,
   background: 'twitter-images-collector_885x450.png',
   msg: '追蹤&同步下載推特用戶圖片'
 };
 oss_projects['OpenAI-Gym-Hearts'] = {
   featured: true,
-  position: 12,
+  position: 13,
   background: 'OpenAI-Gym-Hearts_885x450.jpg',
   msg: '要一起玩牌嗎?'
 };
@@ -93,28 +101,28 @@ oss_projects['stock-minehunter'] = {
   blog_post: 'https://blog.zmcx16.moe/search/label/norn-minehunter%E7%B6%B2%E7%AB%99%E9%96%8B%E7%99%BC',
   online_website: 'https://norn-minehunter.zmcx16.moe/',
   featured: true,
-  position: 13,
+  position: 14,
   background: 'stock-minehunter_885x450.jpg',
   msg: 'Explosion!!!'
 };
 oss_projects['AuctionKira'] = {
   blog_post: 'https://blog.zmcx16.moe/search/label/AuctionKira',
   featured: true,
-  position: 14,
+  position: 15,
   background: 'AuctionKira_885x450.png',
   msg: '(<ゝω·)☆KIRA！'
 };
 oss_projects['ChaldeaStockObservatory'] = {
   blog_post: 'https://blog.zmcx16.moe/search/label/ChaldeaStockObservatory',
   featured: true,
-  position: 15,
+  position: 16,
   background: 'ChaldeaStockObservatory_885x450.jpg',
   msg: '斯大可...是什麼啊?'
 };
 oss_projects['find-house-assistant-chrome-extension'] = {
   blog_post: 'https://blog.zmcx16.moe/search/label/%E5%9C%A8%E5%8F%B0%E7%81%A3%E8%B2%B7%E6%88%BF%E5%AD%90%E6%98%AF%E4%B8%8D%E6%98%AF%E6%90%9E%E9%8C%AF%E4%BA%86%E4%BB%80%E9%BA%BC',
   featured: true,
-  position: 16,
+  position: 17,
   background: 'find-house-assistant-chrome-extension_885x450.jpg',
   msg: '在台灣買房子是不是搞錯了什麼...'
 };
@@ -122,14 +130,14 @@ oss_projects['protobuf-deserializer'] = {
   blog_post: 'https://blog.zmcx16.moe/search/label/protobuf-deserializer%E7%B6%B2%E7%AB%99%E9%96%8B%E7%99%BC',
   online_website: 'https://protobuf-deserializer.zmcx16.moe',
   featured: true,
-  position: 17,
+  position: 18,
   background: 'protobuf-deserializer_885x450.jpg',
   msg: '好像很厲害?'
 };
 oss_projects['MemoOffVocabulary'] = {
   blog_post: 'https://blog.zmcx16.moe/search/label/MemoOffVocabulary',
   featured: true,
-  position: 18,
+  position: 19,
   background: 'MemoOffVocabulary_885x450.jpg',
   msg: '要好好念書喔~'
 };
@@ -137,19 +145,19 @@ oss_projects['zmcx16.github.io'] = {
   blog_post: 'https://blog.zmcx16.moe/search/label/%E5%80%8B%E4%BA%BA%E7%B6%B2%E7%AB%99%E9%96%8B%E7%99%BC',
   online_website: 'https://project.zmcx16.moe',
   featured: true,
-  position: 19,
+  position: 20,
   background: 'zmcx16.github.io_885x450.jpg',
   msg: 'いらっしゃいませ~~~'
 };
 oss_projects['OpenAI-Gym-GongZhu'] = {
   featured: true,
-  position: 20,
+  position: 21,
   background: 'OpenAI-Gym-GongZhu_885x450.jpg',
   msg: '要一起玩牌嗎?'
 };
 oss_projects['Misc'] = {
   featured: true,
-  position: 21,
+  position: 22,
   background: 'Misc_885x450.jpg',
   msg: '聽說是某個大哥哥的黑歷史?'
 };
