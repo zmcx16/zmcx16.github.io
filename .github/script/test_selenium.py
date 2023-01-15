fe9458d5d81cb3568b1e0276edd67003cfc058fd
zmcx16 <zmcx16@gmail.com>
2023-01-15 21:15:25 +0800

    use chromium-browser=108

--- .github/script/test_selenium.py
+++ .github/script/test_selenium.py
@@ -1,35 +1,27 @@
 import logging
 
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-import chromedriver_autoinstaller
 
-chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
-# and if it doesn't exist, download it automatically,
-# then add chromedriver to path
+chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
-chrome_options = webdriver.ChromeOptions()
-# Add your options as needed
+chrome_options = Options()
 options = [
-    # Define window size here
-    "--window-size=1200,1200",
-    "--ignore-certificate-errors"
-
-    # "--headless",
-    # "--disable-gpu",
-    # "--window-size=1920,1200",
-    # "--ignore-certificate-errors",
-    # "--disable-extensions",
-    # "--no-sandbox",
-    # "--disable-dev-shm-usage",
-    # '--remote-debugging-port=9222'
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage"
 ]
-
 for option in options:
     chrome_options.add_argument(option)
 
-driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
 driver.get('https://hk.finance.yahoo.com/quote/BIO?p=BIO')
 logging.info(f'title = {driver.title}')
